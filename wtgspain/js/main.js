b90e5ec568b3f5ad4c587a4f0c26939da1224ce6
front <wtg.frontend@gmail.com>
2017-12-19 18:06:51 +0200

    slider resvonsive now

--- wtgspain/js/main.js
+++ wtgspain/js/main.js
@@ -1,6 +1,3 @@
-/**
- * Created by Brusque on 15.12.2017.
- */
 $('.slick').slick({
     fade: true,
     autoplay: true,
@@ -18,17 +15,77 @@ $('.clients__list').slick({
     slidesToScroll: 1,
     nextArrow: $('.clients__arrow--right'),
     prevArrow: $('.clients__arrow--left'),
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 500,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+    ]
+
 });
 
-$('.item__favorites').on('click', function(){
+$('.staff__list').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: $('.staff__arrow--right'),
+    prevArrow: $('.staff__arrow--left'),
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 500,
+            settings: {
+                slidesToShow: 2
+            }
+        }
+    ]
+});
+
+$('.item__favorites').on('click', function () {
     var icon = $(this).find('.fa');
 
-    if(icon.hasClass('fa-heart-o')){
+    if (icon.hasClass('fa-heart-o')) {
         icon.removeClass('fa-heart-o').addClass('fa-heart');
     }
-    else{
+    else {
         icon.removeClass('fa-heart').addClass('fa-heart-o');
     }
 });
 
+// $('[data-phone]').mask("+38(099)999-99-99");
+
+
 $('select').chosen({disable_search_threshold: 10});
+
+// $('.search__button').on('click', function () {
+//     var search = $('.footer .search__input');
+//
+//     $('body,html').animate({
+//         scrollTop: search.offset().top - 65
+//     }, 500);
+//
+// });
