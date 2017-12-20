e55c0fdeecb3c7b8df1b7ec5fa533dad3e72b2bf
front <wtg.frontend@gmail.com>
2017-12-20 16:08:14 +0200

    new feature for vlog

--- wtgspain/js/main.js
+++ wtgspain/js/main.js
@@ -1,112 +1,124 @@
-$('.slick').slick({
-    fade: true,
-    autoplay: true,
-    arrows: false,
-    autoplaySpeed: 5000,
-    cssEase: 'linear'
-});
+$(document).ready(function () {
 
-$('.clients__list').slick({
-    autoplay: true,
-    autoplaySpeed: 2000,
-    dots: true,
-    pauseOnDotsHover: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    nextArrow: $('.clients__arrow--right'),
-    prevArrow: $('.clients__arrow--left'),
-    responsive: [
-        {
-            breakpoint: 992,
-            settings: {
-                slidesToShow: 2
+    $('.slick').slick({
+        fade: true,
+        autoplay: true,
+        arrows: false,
+        autoplaySpeed: 5000,
+        cssEase: 'linear'
+    });
+
+    $('.clients__list').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        pauseOnDotsHover: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        nextArrow: $('.clients__arrow--right'),
+        prevArrow: $('.clients__arrow--left'),
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 1
+                }
             }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 2
+        ]
+
+    });
+
+    $('.staff__list').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        nextArrow: $('.staff__arrow--right'),
+        prevArrow: $('.staff__arrow--left'),
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 2
+                }
             }
-        },
-        {
-            breakpoint: 500,
-            settings: {
-                slidesToShow: 1
-            }
+        ]
+    });
+
+    $('select').chosen({
+        disable_search_threshold: 10,
+        hide_results_on_select: true,
+        search_contains: true,
+        width: "100%"
+    });
+
+
+    $('.item__favorites').on('click', function () {
+        var icon = $(this).find('.fa');
+
+        if (icon.hasClass('fa-heart-o')) {
+            icon.removeClass('fa-heart-o').addClass('fa-heart');
         }
-    ]
-
-});
-
-$('.staff__list').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    nextArrow: $('.staff__arrow--right'),
-    prevArrow: $('.staff__arrow--left'),
-    responsive: [
-        {
-            breakpoint: 992,
-            settings: {
-                slidesToShow: 3
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 2
-            }
-        },
-        {
-            breakpoint: 500,
-            settings: {
-                slidesToShow: 2
-            }
+        else {
+            icon.removeClass('fa-heart').addClass('fa-heart-o');
         }
-    ]
-});
-
-$('select').chosen({
-    disable_search_threshold: 10,
-    hide_results_on_select: true,
-    search_contains: true,
-    width: "100%"
-});
-
-
-$('.item__favorites').on('click', function () {
-    var icon = $(this).find('.fa');
-
-    if (icon.hasClass('fa-heart-o')) {
-        icon.removeClass('fa-heart-o').addClass('fa-heart');
-    }
-    else {
-        icon.removeClass('fa-heart').addClass('fa-heart-o');
-    }
-});
+    });
 
 // $('[data-phone]').mask("+38(099)999-99-99");
 
+    $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
 
-$('.leave-request').on('click', function (e) {
-    e.preventDefault();
+        fixedContentPos: false
+    });
 
-    var request = $('.request');
+    $('.leave-request').on('click', function (e) {
+        e.preventDefault();
 
-    $('body,html').animate({
-        scrollTop: request.offset().top - 10
-    }, 500);
+        var request = $('.request');
 
-});
+        $('body,html').animate({
+            scrollTop: request.offset().top - 10
+        }, 500);
 
-$(window).scroll(function () {
-    if ($(this).scrollTop() >= 1000) {
-        $('.to-top').fadeIn();
-    } else {
-        $('.to-top').fadeOut();
-    }
-});
-$('.to-top').click(function () {
-    $('body,html').animate({
-        scrollTop: 0
-    }, 800);
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= 1000) {
+            $('.to-top').fadeIn();
+        } else {
+            $('.to-top').fadeOut();
+        }
+    });
+    $('.to-top').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+    });
 });
