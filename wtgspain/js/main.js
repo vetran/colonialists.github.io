0be525f7c62c06154ab97711c0856cdd1885908f
vetran <anatoliy.vetrenko@gmail.com>
2018-01-15 18:06:04 +0200

    add product html

--- wtgspain/js/main.js
+++ wtgspain/js/main.js
@@ -8,6 +8,41 @@ $(document).ready(function () {
         cssEase: 'linear'
     });
 
+    var mainImages = $('.images__main');
+    var thumbImages = $('.images__thumb');
+
+    mainImages.slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false
+    });
+
+    thumbImages.slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        infinite: false
+    });
+    thumbImages.find('.images__item').on('click', function () {
+        mainImages.slick('slickGoTo', $(this).data('slickIndex'));
+    });
+
+    var popupImages = [], images = mainImages.find('.images__item img');
+
+    images.each(function(){
+        popupImages.push({
+            'src' : $(this).attr('src')
+        });
+    });
+
+    images.magnificPopup({
+        items: popupImages,
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
+
     $('.clients__list').slick({
         autoplay: true,
         autoplaySpeed: 2000,
@@ -17,8 +52,7 @@ $(document).ready(function () {
         slidesToScroll: 1,
         nextArrow: $('.clients__arrow--right'),
         prevArrow: $('.clients__arrow--left'),
-        responsive: [
-            {
+        responsive: [{
                 breakpoint: 992,
                 settings: {
                     slidesToShow: 2
@@ -45,8 +79,7 @@ $(document).ready(function () {
         slidesToScroll: 1,
         nextArrow: $('.staff__arrow--right'),
         prevArrow: $('.staff__arrow--left'),
-        responsive: [
-            {
+        responsive: [{
                 breakpoint: 992,
                 settings: {
                     slidesToShow: 3
@@ -86,15 +119,15 @@ $(document).ready(function () {
 
             if (item.hasClass('active')) {
                 item.removeClass('active');
-            }
-            else {
+            } else {
                 searchItems.removeClass('active');
                 item.addClass('active');
             }
         });
 
         function makeChoices(item) {
-            var list = $('.search__choices-list'), elem = '';
+            var list = $('.search__choices-list'),
+                elem = '';
 
             var itemCategory = item.data('category'),
                 itemIndex = item.data('index'),
@@ -103,7 +136,7 @@ $(document).ready(function () {
 
             elem += '<li class="search__choices-item">';
             elem += '    <span class="search__choices-icon search__choices-remove" data-category="' + itemCategory + '" data-index="' + itemIndex + '"><i class="fa fa-times" aria-hidden="true"></i></span>';
-            elem += '    <span class="search__choices-text">' + text +'</span>';
+            elem += '    <span class="search__choices-text">' + text + '</span>';
             elem += '</li>';
 
             list.append(elem);
@@ -112,28 +145,30 @@ $(document).ready(function () {
         form.on('change', function (event) {
             var target = $(event.target);
 
-            if(target.is('[type="checkbox"]') && target.is(':checked')){
-               target.addClass('checked');
-               makeChoices(target);
+            if (target.is('[type="checkbox"]') && target.is(':checked')) {
+                target.addClass('checked');
+                makeChoices(target);
             }
         });
 
-        $(document).on('click','.search__input.checked', function(){ return false });
+        $(document).on('click', '.search__input.checked', function () {
+            return false
+        });
 
-        $(document).on('click','.search__choices-remove', function(){
-            var $this =  $(this),
+        $(document).on('click', '.search__choices-remove', function () {
+            var $this = $(this),
                 category = $this.data('category'),
                 index = $this.data('index');
 
             $this.parent().remove();
 
-            $('input[data-category="'+ category +'"][data-index="'+ index +'"]').removeClass('checked').prop('checked', false);
+            $('input[data-category="' + category + '"][data-index="' + index + '"]').removeClass('checked').prop('checked', false);
 
         });
 
     })();
 
-    $('.news-item__trigger').on('click', function(){
+    $('.news-item__trigger').on('click', function () {
         $(this).remove();
         $('.news-item__wrapper').slideDown('slow');
     });
@@ -143,8 +178,7 @@ $(document).ready(function () {
 
         if (icon.hasClass('fa-heart-o')) {
             icon.removeClass('fa-heart-o').addClass('fa-heart');
-        }
-        else {
+        } else {
             icon.removeClass('fa-heart').addClass('fa-heart-o');
         }
     });
@@ -158,35 +192,40 @@ $(document).ready(function () {
         fixedContentPos: false
     });
 
-    var range = document.getElementById('range');
+    var range = document.getElementById('range') || false;
 
-    noUiSlider.create(range, {
-        start: [0, 6000000],
-        connect: true,
-        range: {
-            'min': [0],
-            '0.1%': [0,  10000 ],
-            "50%": [500000, 100000],
-            'max': [6000000]
-        },
-        format: wNumb({
-            decimals: 0,
-            thousand: ',',
-            postfix: ' €'
-        })
+    if (range) {
+        noUiSlider.create(range, {
+            start: [0, 6000000],
+            connect: true,
+            range: {
+                'min': [0],
+                '0.1%': [0, 10000],
+                "50%": [500000, 100000],
+                'max': [6000000]
+            },
+            format: wNumb({
+                decimals: 0,
+                thousand: ',',
+                postfix: ' €'
+            })
 
-    });
-    range.noUiSlider.on('update', function(values, handle){
-        $('#min-price').text(values[0]);
-        $('#max-price').text(values[1]);
+        });
+        range.noUiSlider.on('update', function (values, handle) {
+            $('#min-price').text(values[0]);
+            $('#max-price').text(values[1]);
 
-        $('[name="min_price"]').val(values[0]);
-        $('[name="max_price"]').val(values[1]);
+            $('[name="min_price"]').val(values[0]);
+            $('[name="max_price"]').val(values[1]);
 
-    });
+        });
+    }
 
+    
     $(window).scroll(function () {
-        if ($(this).scrollTop() >= 1000) {
+        var $this = $(this);
+
+        if ($this.scrollTop() >= 1000) {
             $('.to-top').fadeIn();
         } else {
             $('.to-top').fadeOut();
